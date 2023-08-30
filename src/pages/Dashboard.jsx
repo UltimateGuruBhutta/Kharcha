8281e0bbf9a74740cbd380ff7fda2b3654e5accf
Hamza <hamzaabhutta@gmail.com>
2023-08-30 23:05:44 +0500

    some updates

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,26 @@
 import { useLoaderData } from "react-router-dom";
 import { fetchData } from "../helper";
+import Intro from "../components/Intro";
 
 export function dashboardLoader() {
   const userName = fetchData("userName");
   return { userName };
 }
+
+export async function dashboardAction({request}){
+  const data= await request.formData();
+  const formData=Object.fromEntries(data);
+  localStorage.setItem("userName",JSON.stringify(formData.userName))
+
+}
+
 const Dashboard = () => {
   const { userName } = useLoaderData();
    
 
   return <div>
-    <h1>{userName}</h1>
-    
-    Dashboard</div>;
+   {userName ? (<p>{userName}</p>):(<Intro/>)}
+    </div>;
 };
 
 export default Dashboard;
