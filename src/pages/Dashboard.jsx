f3179d1471952dfbd9ed33b6df8008ead2f0532f
Hamza <hamzaabhutta@gmail.com>
2023-08-31 20:53:48 +0500

    Add Form to create Budget

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,33 +2,50 @@ import { useLoaderData } from "react-router-dom";
 import { fetchData } from "../helper";
 import Intro from "../components/Intro";
 import { toast } from "react-toastify";
+import AddBudgetForm from "../components/AddBudgetForm";
 
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  return { userName };
+  const budgets = fetchData("budgets");
+
+  return { userName, budgets };
 }
 
-export async function dashboardAction({request}){
-  const data= await request.formData();
-try{
-  throw new Error(" heilo")
-  const formData=Object.fromEntries(data);
-  localStorage.setItem("userName",JSON.stringify(formData.userName))
-return toast.success(`Welcome , ${formData.userName}`)
-}
-catch(e){
-  throw new Error("There was a problem creating your account.")
-
-}
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  try {
+    const formData = Object.fromEntries(data);
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome , ${formData.userName}`);
+  } catch (e) {
+    throw new Error("There was a problem creating your account.");
+  }
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData();
-   
+  const { userName, budgets } = useLoaderData();
 
-  return <div>
-   {userName ? (<p>{userName}</p>):(<Intro/>)}
-    </div>;
+  return (
+    <div>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome back, <span className="accent">{userName} </span>
+          </h1>
+          <div className="grid-sm">
+
+            { budgets  }
+            <div className="grid-lg"></div>
+            <div className="flex-lg">
+              <AddBudgetForm/>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
