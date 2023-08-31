67f233a4af12602f355e0e0b2ea1cdf2f026b82f
Hamza <hamzaabhutta@gmail.com>
2023-08-31 20:28:05 +0500

    Create Errror handling page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import { fetchData } from "../helper";
 import Intro from "../components/Intro";
+import { toast } from "react-toastify";
 
 export function dashboardLoader() {
   const userName = fetchData("userName");
@@ -9,9 +10,16 @@ export function dashboardLoader() {
 
 export async function dashboardAction({request}){
   const data= await request.formData();
+try{
+  throw new Error(" heilo")
   const formData=Object.fromEntries(data);
   localStorage.setItem("userName",JSON.stringify(formData.userName))
+return toast.success(`Welcome , ${formData.userName}`)
+}
+catch(e){
+  throw new Error("There was a problem creating your account.")
 
+}
 }
 
 const Dashboard = () => {
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useRouteError, Link, useNavigate } from "react-router-dom";
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 
 function Error() {
+  const error = useRouteError();
+  const navigate = useNavigate();
   return (
-    <div>Error!!</div>
-  )
+    <div className="error">
+      <h1> Uh oh! We've got a problem.</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
+          {" "}
+          Go Back <ArrowUturnLeftIcon width={20} />
+        </button>
+
+        <Link to="/" className="btn btn--dark">
+          <HomeIcon width={20} />
+          <span>Go Home </span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
